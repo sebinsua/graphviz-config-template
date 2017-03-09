dedff1d6ed3852c967834ddd5eb0dd95f8962988
Seb Insua <me@sebinsua.com>
2017-03-09 01:59:21 +0000

    test(graph-config): passing tests; not sure if that is good

--- test/graph-config.js
+++ test/graph-config.js
@@ -1,14 +1,14 @@
 const test = require('ava')
 
 const graphConfig = require('../src')
+const { using } = graphConfig
 
 const { digraph, graph } = graphConfig()
 
 // TODO: Write usage examples for imaginary implementation.
 // TODO: Test edge-cases.
 
-test('can represent a simple graph', t => {
-  /*
+test('can represent simple graph', t => {
   const save = graph`
     A -> B [label="LIKES"];
     A -> C [label="LOVES"];
@@ -17,41 +17,38 @@ test('can represent a simple graph', t => {
   const statements = save({
     A: { id: 10, body: 'body text' },
     B: { id: 25 },
-    C: { type: [ 'C', 'LABEL_OF_C' ], id: 100 }
+    C: { type: ['C', 'LABEL_OF_C'], id: 100 }
   })
-   */
+
+  t.snapshot(statements)
 })
 
-test('can represent a complex graph with one-to-many relationships, transformations, etc', t => {
-  /*
-  // NOTE: labels on nodes and relationships: http://stackoverflow.com/a/6055235
+test('can represent complex graph (1-to-m, transformations)', t => {
   const A_1 = { sides: 3 }
-  const A_2 = { sides: 5 }
   const B = { sides: 5 }
-  const C = { sides: 8 }
 
   const typeToLabel = props => ({ label: props.type, id: props.id })
   const save = digraph`
-    A [idName=aId,${A_1}];
-    B [idName=bId,${B}]
-    C [idName=id,${using('C')(typeToLabel)}];
-    D [idName=id,${using('D')(typeToLabel)}];
+  A [idName=aId,${A_1}];
+  B [idName=bId,${B}]
+  C [idName=id,${using('C')(typeToLabel)}];
+  D [idName=id,${using('D')(typeToLabel)}];
 
-    A -> B [label="LIKES"];
-    A -> C -> D [label="LOVES"];
+  A -> B [label="LIKES"];
+  A -> C -> D [label="LOVES"];
   `
 
   const statements = save({
-    A: { id: 10, body: 'body text' }, // [ { aId: 10 }, { aId: 15 }, { aId: 20 } ],
-    B: [ { id: 25 }, { id: 30 }, { id: 35 }, { id: 40 }, { id: 45 } ],
-    C: { type: [ 'C', 'LABEL_OF_C' ], id: 100 },
-    D: { type: [ 'D', 'LABEL_OF_D' ], id: 500 }
+    A: { aId: 10, body: 'body text' }, // [ { aId: 10 }, { aId: 15 }, { aId: 20 } ],
+    B: [{ bId: 25 }, { bId: 30 }, { bId: 35 }, { bId: 40 }, { bId: 45 }],
+    C: { type: ['C', 'LABEL_OF_C'], id: 100 },
+    D: { type: ['D', 'LABEL_OF_D'], id: 500 }
   })
-  */
+
+  t.snapshot(statements)
 })
 
 test('can switch the direction of the relationships', t => {
-  /*
   const save = digraph`
     A -> B [dir="forward",label="LIKES"];
     A -> C -> D [dir="back",label="LOVES"];
@@ -59,16 +56,16 @@ test('can switch the direction of the relationships', t => {
 
   const statements = save({
     A: { id: 10, body: 'body text' }, // [ { aId: 10 }, { aId: 15 }, { aId: 20 } ],
-    B: [ { id: 25 }, { id: 30 }, { id: 35 }, { id: 40 }, { id: 45 } ],
-    C: { type: [ 'C', 'LABEL_OF_C' ], id: 100 },
-    D: { type: [ 'D', 'LABEL_OF_D' ], id: 500 }
+    B: [{ id: 25 }, { id: 30 }, { id: 35 }, { id: 40 }, { id: 45 }],
+    C: { type: ['C', 'LABEL_OF_C'], id: 100 },
+    D: { type: ['D', 'LABEL_OF_D'], id: 500 }
   })
-   */
+
+  t.snapshot(statements)
 })
 
 test('can represent nodes connected to themselves', t => {
-  /*
-  const selfConnected = digraph`
+  const save = digraph`
     opinion
 
     fact_1 [label="fact"]
@@ -76,5 +73,12 @@ test('can represent nodes connected to themselves', t => {
 
     fact_1 -> fact_2
   `
-  */
+
+  const statements = save({
+    opinion: { id: 1, message: 'this is probably broken' },
+    fact_1: { id: 101, message: '1 + 1 = 2' },
+    fact_2: { id: 9000, message: 'snapshots are cool' }
+  })
+
+  t.snapshot(statements)
 })
